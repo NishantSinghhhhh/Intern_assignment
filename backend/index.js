4b8f89a2333db41ca2212c3524526e672a14e5c5
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2024-09-22 23:10:23 +0530

    going for deployment-13

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(cors({
   origin: 'https://intern-assignment-frontend.vercel.app',
   methods: ['GET', 'POST', 'OPTIONS'], // Add OPTIONS
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
 app.use(express.json());
